dd1a96e0bc9d8856f683a5af0c1c5f87329a85c4
Md Tabraiz <mohammadtabraiz3@gmail.com>
2024-11-14 19:06:37 +0530

    update in uri

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,19 @@ app.use(express.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
-mongoose.connect("mongodb://localhost:27017/carApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(()=>{console.log('database connection succesfull')
-  seedData();
-}).catch(()=>console.log('error in database conection'));
+mongoose
+  .connect(
+    "mongodb+srv://tabraiz:tabraiz@cluster0.76smate.mongodb.net/car-management?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("database connection succesfull");
+    seedData();
+  })
+  .catch(() => console.log("error in database conection"));
 app.get('/', (req,res)=>{
   res.send('welcome to server')
 })
